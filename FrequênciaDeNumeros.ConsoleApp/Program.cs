320338dac9453e2b27ce77f5dd8c1faa84771eb7
usuario <usuario@DESKTOP-CNTKCC8>
2022-03-03 02:39:10 -0300

    Add project files.

---
+++ FrequênciaDeNumeros.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FrequênciaDeNumeros.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 8, 10, 8, 260, 4, 10, 10 };
+
+            Array.Sort(array);
+
+            int i = 0;
+            int j = 0;
+
+            while (i < array.Length)
+            {
+                int numero = array[i];
+                int quantidadeVezes = 0;
+
+                while (array[i] == array[j])
+                {
+                    quantidadeVezes++;
+                    j++;
+
+                    if (j == array.Length)
+                        break;
+                }
+
+                string sufixo = (quantidadeVezes > 1) ? " vezes" : " vez";
+
+                Console.WriteLine(numero + " apareceu " + quantidadeVezes + sufixo);
+
+                i = j;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
